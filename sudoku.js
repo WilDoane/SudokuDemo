04825c2367ad415cd11ad76bde1ee9120002523d
William Doane <wil@DrDoane.com>
2012-05-09 15:34:04 -0400

    invert assertion logic

--- sudoku.js
+++ sudoku.js
@@ -1,15 +1,14 @@
 var assert = function(expression) {
-  if (eval(expression) !== true) {   
-    throw "An assertion failed: " + expression;
-  } else {
+  if (eval(expression) === true) {   
     console.log("PASSED: " + expression);
+  } else {
+    throw "An assertion failed: " + expression;
   }
 }
 
 var board = "007090058090500000008306709600040231000162000712050004201907500000001080870030400";
 
 
-
 assert("1 == 1");
-assert("1 == 2");
+assert("2 == 2");
 
