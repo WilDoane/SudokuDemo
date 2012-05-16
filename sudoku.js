7b5c84b58ce9077337af413a20152d32c6b3d337
William Doane <wil@DrDoane.com>
2012-05-16 01:19:04 -0400

    Add row column based get and set methods

--- sudoku.js
+++ sudoku.js
@@ -126,6 +126,38 @@ var isUsedInColumn = function(colNumber, digit) {
   return result;
 }
 
+/*
+
+  Array index numbers (i)
+   r   c 1  2  3    4  5  6    7  8  9
+   1     0  1  2    3  4  5    6  7  8
+   2     9 10 11   12 13 14   15 16 17
+   3    18 19 20   21 22 23   24 25 26
+       
+   4    27 28 29   30 31 32   33 34 35
+   5    36 37 38   39 40 41   42 43 44
+   6    45 46 47   48 49 50   51 52 53
+       
+   7    54 55 56   57 58 59   60 61 62
+   8    63 64 65   66 67 68   69 70 71
+   9    72 73 74   75 76 77   78 79 80
+
+   r = floor(i / 9) + 1
+   c = (i % 9) + 1
+
+   i = ((r - 1) * 9) + (c - 1)
+
+*/
+
+var get = function(r, c) {
+  var i = ((r - 1) * 9) + (c - 1);
+  return board[i];
+}
+
+var set = function(r, c, v) {
+  var i = ((r - 1) * 9) + (c - 1);
+  board = spliceString(board, v.toString(), i);  
+}
 
 var spliceString = function(str, char, pos) {
   /*
